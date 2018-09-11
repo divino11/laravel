c70a9c7c9fa7d85308b5ac93787d186146f871ac
unknown <a.vasyukov97@gmail.com>
2018-09-11 15:34:09 +0300

    added create categories page

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Self_;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    // Mass assigned
+    protected $fillable = ['title', 'slug', 'parent_id', 'published', 'created_by', 'modified_by'];
+
+    // Mutators
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = Str::slug(mb_substr($this->title, 0, 40) . "-" . \Carbon\Carbon::now()->format('dmiHi'), '-');
+    }
+
+    //Get children category
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
